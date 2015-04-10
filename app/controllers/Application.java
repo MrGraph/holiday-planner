ee270c165481cd961b3aafbb399653a5b4677b38
MrGraph <mattklawonn@gmail.com>
2015-04-10 13:21:06 -0400

    Added vacations action to application, linked things to vacation page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,9 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Holiday Planner"));
     }
+    
+    public static Result vacations() {
+    	return ok(vacations.render());
+    }
 
 }
