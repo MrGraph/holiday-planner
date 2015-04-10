d110c91833af138025a24e5398ac91e7a49ecca7
MrGraph <mattklawonn@gmail.com>
2015-04-09 22:50:02 -0400

    Edited views, added photos

---
+++ app/models/Vacation.java
@@ -0,0 +1,17 @@
+package models;
+
+
+
+public class Vacation {
+	
+	public String location;
+	public String start_date;
+	public String end_date;
+	public Integer price;
+	
+	public Vacation() {
+		location = new String();
+	}
+	
+	
+}
