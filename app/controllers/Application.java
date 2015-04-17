ad0d3ffe822ea24bc126e5ed00f5c3088e09aa26
MrGraph <mattklawonn@gmail.com>
2015-04-17 12:33:42 -0400

    Now responds to form input with simple display

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,15 @@
 package controllers;
 
+import java.util.ArrayList;
+
+import models.Vacation;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.formdata.VacationFormData;
-import views.html.index;
-import views.html.planner;
-import views.html.vacations;
+import views.html.*;
+//import views.html.planner;
+//import views.html.vacations;
 
 public class Application extends Controller {
 
@@ -29,9 +32,17 @@ public class Application extends Controller {
     	
     	VacationFormData data = boundForm.get();
     	
+    	
+    	ArrayList<Vacation> vacation_list = new ArrayList<Vacation>();
     	//TODO Check for specific keywords, etc, to redirect to the top recommended vacation
     	//for the user.
+    	if (data.location.equals("Hawaii")){
+    		ArrayList<String> keywords1 = new ArrayList<String>();
+    		keywords1.add("Beach"); keywords1.add("relaxing");
+    		vacation_list.add(new Vacation("Hawaii", "05-22-2015", "05-31-2015", "$2000", keywords1));
+    	}
     	
-    	return ok(data.location);
+    	
+    	return ok(suggestions.render(vacation_list));
     }
 }
--- app/models/Vacation.java
+++ app/models/Vacation.java
@@ -1,17 +1,26 @@
 package models;
 
-
+import java.util.ArrayList;
 
 public class Vacation {
 	
 	public String location;
 	public String start_date;
 	public String end_date;
-	public Integer max_price;
+	public String the_cost;
+	public ArrayList<String> keywords = new ArrayList<String>();
 	
-	public Vacation() {
-		location = new String();
+	public Vacation() {}
+	
+	public Vacation(String location, String start_date, String end_date, String the_cost,
+					ArrayList<String> keywords){
+		this.location = location;
+		this.start_date = start_date;
+		this.end_date = end_date;
+		this.the_cost = the_cost;
+		for (String key : keywords){
+			//if (key == null) continue;
+			this.keywords.add(key);
+		}
 	}
-	
-	
 }
